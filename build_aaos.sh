f5ca89dacc87564aba295a3d928c5f99006b4760
Zach Dean <zach.dean@daveramsey.com>
2023-10-23 19:12:18 +0400

    add build script

---
+++ build_aaos.sh
@@ -0,0 +1,20 @@
+# set android build env
+sudo apt-get update
+sudo apt-get install -y repo
+sudo apt-get install -y git-core gnupg flex bison build-essential zip curl zlib1g-dev libc6-dev-i386 libncurses5 x11proto-core-dev libx11-dev lib32z1-dev libgl1-mesa-dev libxml2-utils xsltproc unzip fontconfig
+
+# install additional packages
+sudo apt-get install -y bc coreutils dosfstools e2fsprogs fdisk kpartx mtools ninja-build pkg-config python3-pip
+sudo pip3 install --yes meson mako jinja2 ply pyyaml
+
+# init repo
+repo init -u https://android.googlesource.com/platform/manifest -b android-13.0.0_r35 --depth=1
+git clone https://github.com/grapeup/aaos_local_manifest.git .repo/local_manifests
+
+repo sync
+
+. build/envsetup.sh
+lunch aosp_rpi4-userdebug
+make bootimage systemimage vendorimage
+
+./rpi4-mkimg.sh
